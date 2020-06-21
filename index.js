f6e69e70805be2a8e32c5cf52c83bc7fc34cb3f7
Tuukka <tuukkaveteli@gmail.com>
2020-06-21 23:16:09 +0300

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ morgan.token('object', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
 app.use(cors())
 app.use(express.static('build'))
-
+//
 
 let phonebook = [
     {
