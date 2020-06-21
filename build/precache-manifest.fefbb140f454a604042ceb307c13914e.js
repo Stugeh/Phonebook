2be5f7939f829b3cc0b9f7990a991ce3f71f7797
Tuukka <tuukka@pop-os.localdomain>
2020-06-21 21:42:13 +0300

    uibuild

---
+++ build/precache-manifest.fefbb140f454a604042ceb307c13914e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c860517d2a2df48c99ccdddc6577bba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b792a3995fcfb5e4a6",
+    "url": "/static/css/main.6e5520d0.chunk.css"
+  },
+  {
+    "revision": "0d0e4a7843afe9a3c41d",
+    "url": "/static/js/2.118405a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.118405a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b792a3995fcfb5e4a6",
+    "url": "/static/js/main.1229f0be.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
