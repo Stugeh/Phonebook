f039566f2d7910996c9e4e0785c520a20e5f51ac
Stugeh <tuukkaveteli@gmail.com>
2020-06-25 16:49:13 +0300

    deleting and posting to database via the frontend works.

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-app.use(express.json())
 morgan.token('object', req => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
 app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
+app.use(logger)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
 const Person = require('./models/person')
 
 // let phonebook = [
@@ -53,15 +54,18 @@ app.get('/api/persons', (req, res) => {
 })
 
 // individual person.
-app.get('/api/persons/:_id', (req, res) => {
-    Person.findById(req.params._id).then(person => {
-        console.log('person :>> ', person)
-        if (person) {
-            res.json(person.toJSON)
-        } else {
-            res.status(404).end()
-        }
-    })
+app.get('/api/persons/:id', (req, res, next) => {
+    console.log("finding individual..")
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                console.log('getting person :>> ', person);
+                res.json(person)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(err => next(err))
 })
 
 // add person.
@@ -84,21 +88,37 @@ app.post('/api/persons', (req, res) => {
 
     const person = new Person({
         name: body.name,
-        number: body.number,
-        id: Math.floor(Math.random() * 1000000000)
+        number: body.number
     })
     person.save().then(result => {
         console.log(`added ${result.name} number ${result.number} to the phonebook`)
-        res.json(person.toJSON)
-
+        res.json(person)
     })
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    console.log('person :>> ', person);
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(newPerson => {
+            res.json(newPerson)
+        })
+        .catch(err => next(err))
+})
+
 // delete person.
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    phonebook = phonebook.filter(note => note.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    console.log(req.params)
+    Person.findByIdAndDelete(req.params.id)
+        .then(person => {
+            console.log('deleting person :>>', person)
+            res.status(204).end()
+        })
+        .catch(err => next(err))
 })
 
 
